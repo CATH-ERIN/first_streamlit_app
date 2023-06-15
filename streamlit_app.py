14d7acd34f1a39fb67568c36c16197f3154f8822
CATH-ERIN <136728785+CATH-ERIN@users.noreply.github.com>
2023-06-15 21:04:16 +0530

    Update streamlit_app.py
    Fixed errors on '

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
 streamlit.title('My Moms New Healthy Diner')
-streamlit.header(🥑'Breakfast Favourites')
-streamlit.text(🥣'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗'Kale , Spinach & Rocket Smoothie')
-streamlit.text(🐔'Hard Boiled Free-Range Egg')
+streamlit.header('🥑 Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale , Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
