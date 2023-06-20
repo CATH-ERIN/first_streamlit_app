fbfa25d93c192eb9e8a4e769e2f4ae097e1a7bea
CATH-ERIN <136728785+CATH-ERIN@users.noreply.github.com>
2023-06-20 12:01:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 def insert_row_snowflake(new_fruit):
  with my_cnx.cursor() as my_cur:
    my_cur.execute("insert into fruit_load_list values('" + new_fruit + "')")
-    return "Thanks for adding " + add_my_fruit
+   return "Thanks for adding " + add_my_fruit
 
 #if streamlit.button('Add a Fruit of the List'):
   
