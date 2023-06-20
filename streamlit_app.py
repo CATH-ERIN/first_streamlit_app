c579bf05ec9c016f99314cf75572f24f8d3c70e7
CATH-ERIN <136728785+CATH-ERIN@users.noreply.github.com>
2023-06-20 14:58:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ except URLError as e:
 #import requests
 
 
-streamlit.stop()
+
 
 #import snowflake.connector
 
@@ -67,7 +67,7 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
-
+streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 def insert_row_snowflake(new_fruit):
  with my_cnx.cursor() as my_cur:
