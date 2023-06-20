1633cb3018c0827a84ff19ab05883a7217cb5833
CATH-ERIN <136728785+CATH-ERIN@users.noreply.github.com>
2023-06-20 14:46:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,15 +63,10 @@ def get_fruit_load_list():
          return my_cur.fetchall()
 
 # Add a buttton to load the fruit
-if streamlit.button('Get Fruit Load List')
+if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
-
-
-
-
-
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 def insert_row_snowflake(new_fruit):
